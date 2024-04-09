409023a05d4bc0e5945e32941990e634df37e643
AlexNush <nushtaikin@yandex.ru>
2024-04-09 17:54:25 +0300

    Add app

---
+++ mysite/shopapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+def shop_index(request: HttpRequest):
+    print(request.path)
+    print(request.method)
+    print(request.headers)
+    return HttpResponse('<h1>Hello World!<h1>')
